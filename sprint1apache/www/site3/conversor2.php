b7c7e6085c4881e68697b53effebcd4f6379345b
cristina <mmaceirass@fpcoruna.afundacion.org>
2022-09-29 12:23:04 +0200

    Ejercicio 6 sin completar

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -1,5 +1,4 @@
 <html>
-
 	<body>
 		<h1>Conversor de longitudes</h1>
 
@@ -15,6 +14,14 @@
 					} else {
 						echo "Unidad no soportada";
 					}
+
+					if ($_POST["funidad"] == "yarda"){
+						$v_yardas = $_POST["fcantidad"];
+						$v_metros = $v_yardas * 0.9144;
+						echo $v_yardas. " yardas(s) = ".$v_metros." metros(s)";
+					} else {
+						echo "Unidad no soportada";
+					}
 				}
 			?>
 		</p>
@@ -24,10 +31,14 @@
 		<form action="/conversor2.php" method="post">
 			<label for="cantidad_input">Cantidad:</label><br>
 			<input type="text" id="cantidad_input" name="fcantidad"><br>
+
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for="yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
+
 			<input type="submit" value="Convertir">
 		</form>
 	</body>
