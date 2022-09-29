32bf3c32aab9c7da35bcb483b75b02f58724eb43
cristina <mmaceirass@fpcoruna.afundacion.org>
2022-09-29 10:43:38 +0200

    Completado ejercicio 3 y corregido errores

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,14 @@
+<html>
+	<meta charset="UTF-8">
+
+	<body>
+		<h1>Página de bienvenidas</h1>
+		<?php
+			function dar_bienvenida($nombre) {
+				echo "¡Bienvenido/a, " . $nombre . "!";
+			}
+
+			dar_bienvenida("Matthew Perry");
+		?>
+	</body>
+</html>
