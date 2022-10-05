e1d26368fe3cf4d6de2b151893d1935523a829d0
cristina <mmaceirass@fpcoruna.afundacion.org>
2022-10-05 14:55:00 +0200

    visualizacion de la tabla sin terminar

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,5 +1,41 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<body>
+		<h1>Conexión establecida</h1>
+		<?php
+			$query = 'SELECT * FROM tCanciones';
+			$result = mysqli_query($db, $query) or die('Query error');
 
+			echo '<table border="5">';
+			echo '<tr>';
+			echo '<th>ID</th>';
+			echo '<th>Nombre</th>';
+			echo '<th>Artista</th>';
+			echo '<th>Año</th>';
+			echo '<th>Foto</th>';
+			echo '</tr>';
+
+			while ($row = mysqli_fetch_array($result)) {
+				echo '<tr>';
+				echo '<td>'.$row['id'].'</td>';
+				echo '<td>'.$row['nombre'];
+				echo '</a>';
+				echo ' ';
+				echo $row['artista'];
+				echo ' ';
+				echo $row['anho'];
+				echo '<br><br>';
+				echo '<img src="';
+				echo $row['url_imagen'];
+				echo '" width ="300" height="300"/>';
+				echo '<br><br>';
+			}
+
+			echo '</table>';
+
+			mysqli_close($db);
+		?>
+	</body>
+</html>
