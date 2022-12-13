fc37082aff8ba14c45888a9a80b545464d58fc87
cristina <mmaceirass@fpcoruna.afundacion.org>
2022-12-13 14:02:58 +0100

    añadido el zip y comienzo del pdf

--- opcional_pdf/pagina.php
+++ opcional_pdf/pagina.php
@@ -1,9 +1,13 @@
 <?php
+	$nombre = $_POST['name'];
+	$apell = $_POST['ape'];
+
 	require('fpdf.php');
 
 	$pdf=new FPDF();
 	$pdf->AddPage();
 	$pdf->SetFont('Arial','B',16);
-	$pdf->Cell(40,10,'hola mundo');
-	$pdf->Output('prueba.pdf');
+	$pdf->Cell(190,10,'Diploma de Desarrollo Web en Entorno Servidor', 1, 2, 'C');
+	$pdf->Cell(50,10,'Alumno/a: '.$nombre.' '.$apell);
+	$pdf->Output();
 ?>
