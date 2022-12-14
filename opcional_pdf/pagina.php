f2d7e0414932471d94af291017d83d487dc2a698
cristina <mmaceirass@fpcoruna.afundacion.org>
2022-12-14 14:40:21 +0100

    Terminado el ejercicio opcional de pdf

--- opcional_pdf/pagina.php
+++ opcional_pdf/pagina.php
@@ -7,23 +7,31 @@
 
 	class PDF extends FPDF {
 		function Header() {
-			$this->SetFont('Arial', 'B', 15);
-			$this->Cell(80);
-			$this->Cell(30,10, 'Title', 1, 0, 'C');
-			$this->Ln(20);
+			$this->Image('lazo.png', 7,1,14);
+			$this->Image('gorro.png',185,7,25);
+			$this->SetFont('Arial','BU',20);
+			$this->Cell(190,10,'Diploma de Desarrollo Web en Entorno Servidor', 0,2,'C');
+			$this->Ln(5);
+		}
+
+		function Footer() {
+			$this->SetY(-15);
+			$this->SetFont('Arial', 'B', 8);
+			$this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
 		}
 	}
 
-	$pdf=new FPDF();
+	$pdf=new PDF();
 	$pdf->AddPage();
-	$pdf->Image('lazo.png', 7,1,14);
-	$pdf->Image('gorro.png',185,7,25);
-	$pdf->SetFont('Arial','BU',20);
-	$pdf->Cell(190,10,'Diploma de Desarrollo Web en Entorno Servidor', 0,2,'C');
-	$pdf->Ln(5);
-
 	$pdf->SetFont('Times','',16);
-	$pdf->Cell(50,10,'Alumno/a: '.$nombre.' '.$apell, 0,2);
-	$pdf->Cell(10,10,'Fecha del diploma: '.$fecha);
+	$pdf->Cell(190,10,'Alumno/a: '.$nombre.' '.$apell,0,2,'C');
+	$pdf->Ln(5);
+	$pdf->SetFont('Times','',12);
+	$pdf->Cell(10,5,'El alumno agraciado con este diploma consiguio superar todas las practicas supuestas en esta asignatura',0,1);
+	$pdf->Cell(10,5,'y logro aprobarla, con mucho orgullo le obsequiamos con este diploma',0,2);
+	$pdf->Cell(10,5,'Enhorabuena');
+	$pdf->Ln(10);
+	$pdf->Cell(0,10,'Fecha del diploma: '.$fecha,0,0,'R');
+	$pdf->AliasNbPages();
 	$pdf->Output();
 ?>
